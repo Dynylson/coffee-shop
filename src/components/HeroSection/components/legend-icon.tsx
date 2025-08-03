96888f371785ae56a407c8bc8cadbc75cc2a430f
Dynylson Júnior <dynylsonjr@gmail.com>
2025-08-02 22:52:02 -0300

    feat: add LegendIcon component

--- src/components/HeroSection/components/legend-icon.tsx
+++ src/components/HeroSection/components/legend-icon.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image';
 
-export const LegendIcon = () => {
+interface LegendIconProps {
+  icon: string;
+  backgroundColor: string;
+}
+
+export const LegendIcon = ({ icon, backgroundColor }: LegendIconProps) => {
   return (
-    <div>
-      <Image />
+    <div
+      className={`flex items-center justify-center p-2 rounded-full`}
+      style={{ backgroundColor }}
+    >
+      <Image src={icon} width={13} height={13} alt="" />
     </div>
   );
 };
