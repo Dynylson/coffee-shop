8203aad78ecfeff8d2b650890a6511d544c0a8ef
Dynylson Júnior <dynylsonjr@gmail.com>
2025-08-02 22:52:45 -0300

    feat: finish HeroSection component

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,26 +1,39 @@
 import { ReactNode } from 'react';
 import { Legend } from './components/legend';
-import { ILegend } from './types';
+import Image from 'next/image';
 
 interface HeroSectionProps {
   title: ReactNode;
   subtitle: string;
-  legends: ILegend[];
+  legends: {
+    icon: ReactNode;
+    description: string;
+  }[];
 }
 
 export const HeroSection = ({ title, subtitle, legends }: HeroSectionProps) => {
   return (
-    <section>
-      <h1 className="font-baloo font-bold text-base-title text-baloo-xl leading-tight">
-        {title}
-      </h1>
-      <p className="text-roboto-lg text-base-subtitle max-w-[50ch] mt-4">
-        {subtitle}
-      </p>
-      <div className="mt-[66px]">
-        {legends.map(({ icon, description }, index) => (
-          <Legend key={index} icon={icon} description={description} />
-        ))}
+    <section className="flex justify-between">
+      <div>
+        <h1 className="font-baloo font-bold text-base-title text-baloo-xl leading-tight">
+          {title}
+        </h1>
+        <p className="text-roboto-lg text-base-subtitle max-w-[50ch] mt-4">
+          {subtitle}
+        </p>
+        <div className="grid grid-cols-2 gap-4 mt-[66px]">
+          {legends.map(({ icon, description }, index) => (
+            <Legend key={index} icon={icon} description={description} />
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <Image
+          src="/coffee-delivery.png"
+          width={476}
+          height={360}
+          alt="Coffee Delivery Banner"
+        />
       </div>
     </section>
   );
